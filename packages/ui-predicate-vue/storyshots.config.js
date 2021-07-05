63bbce8bcdabd6ae7c7ab82190f2e78f0a0a0fe4
Romain Baumier <baumier.romain@gmail.com>
2021-07-05 09:16:58 +0200

    feat(vue storybook): upgrade to storybook @storybook/vue v6

--- packages/ui-predicate-vue/storyshots.config.js
+++
@@ -1,9 +0,0 @@
-import path from 'path';
-import { multiSnapshotWithOptions } from '@storybook/addon-storyshots';
-
-export default {
-  framework: 'vue',
-  configPath: path.join(__dirname, '.storybook'),
-  integrityOptions: { cwd: path.join(__dirname, 'src') },
-  test: multiSnapshotWithOptions({}),
-};
--- packages/ui-predicate-vue/vueshots._test_.js
+++
@@ -1,3 +0,0 @@
-import initStoryshots from '@storybook/addon-storyshots';
-import config from './storyshots.config';
-initStoryshots(config);
